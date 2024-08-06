4a97525ba5e79412c2e895a8d122e603936c7f34
venu-prasath <venuprasth133@gmail.com>
2024-08-06 03:47:36 -0400

    declarative navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -41,10 +41,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
