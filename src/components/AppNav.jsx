5361047f5b59aa5e27f4ebd5c7345bae518ef236
venu-prasath <venuprasth133@gmail.com>
2024-08-05 21:30:49 -0400

    first commit

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,7 @@
+import { nav } from "./AppNav.module.css";
+
+function AppNav() {
+  return <div className={nav}>App navigation</div>;
+}
+
+export default AppNav;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h3>App </h3>
+    </div>
+  );
+}
+
+export default AppLayout;
